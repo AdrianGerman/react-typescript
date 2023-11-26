c630a5389b66c52bc8b4fdf65b8e73b77cf1c987
AdrianGerman <adrian.german1019@gmail.com>
2023-11-26 10:16:58 -0600

    props y children

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,7 +1,7 @@
 // generate a random function between 1 and 123
-const random = (): number => Math.floor(Math.random() * 123) + 1;
 
-export const RandomFox = (): JSX.Element => {
-  const image: string = `https://randomfox.ca/images/${random()}.jpg`;
+type Props = { image: string };
+
+export const RandomFox = ({ image }: Props): JSX.Element => {
   return <img className="rounded-2xl" width={320} height="auto" src={image} />;
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,12 @@ import { Inter } from "next/font/google";
 import Head from "next/head";
 import { RandomFox } from "@/components/RandomFox";
 
+// Es para la fuente de la tipografia, con la lib de google
 const inter = Inter({ subsets: ["latin"] });
+
+// El llamado a la random fox
+const random = (): number => Math.floor(Math.random() * 123) + 1;
+// const image: string = `https://randomfox.ca/images/${random()}.jpg`;
 
 export default function Home() {
   return (
@@ -13,7 +18,7 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">Hola tecnomundo!</h1>
-        <RandomFox />
+        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
       </main>
     </div>
   );
