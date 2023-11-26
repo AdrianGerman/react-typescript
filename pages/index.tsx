2418ed4fd82dadfe826fc450c20f4a8863427903
AdrianGerman <adrian.german1019@gmail.com>
2023-11-26 10:33:51 -0600

    state with primitive types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import { RandomFox } from "@/components/RandomFox";
@@ -7,9 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 
 // El llamado a la random fox
 const random = (): number => Math.floor(Math.random() * 123) + 1;
-// const image: string = `https://randomfox.ca/images/${random()}.jpg`;
 
 export default function Home() {
+  const [images, setImages] = useState<Array<string>>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
   return (
     <div>
       <Head>
@@ -18,7 +24,11 @@ export default function Home() {
       </Head>
       <main>
         <h1 className="text-3xl font-bold underline">Hola tecnomundo!</h1>
-        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />
+        {images.map((image, index) => (
+          <div key={index} className="p-4">
+            <RandomFox image={image} />
+          </div>
+        ))}
       </main>
     </div>
   );
