9783a0395ea685cdb7aac21cb067ce5d9788d03f
AdrianGerman <adrian.german1019@gmail.com>
2023-11-27 10:33:18 -0600

    Lazy loading

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,10 +1,33 @@
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 
 // generate a random function between 1 and 123
 type Props = { image: string };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
   const node = useRef<HTMLImageElement>(null);
+  const [src, setSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
 
-  return <img ref={node} className="rounded-lg" width={320} height="auto" src={image} />;
+  useEffect(() => {
+    //Nuevo observador
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setSrc(image);
+        }
+      });
+    });
+
+    if (node.current) {
+      // observe node
+      observer.observe(node.current);
+    }
+    // desconectar
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
+
+  return <img ref={node} className="rounded-lg bg-gray-400" width={320} height="auto" src={src} />;
 };
