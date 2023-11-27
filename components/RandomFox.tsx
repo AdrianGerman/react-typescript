fc19143e12761a2f1673066cfce4ad6511df51a3
AdrianGerman <adrian.german1019@gmail.com>
2023-11-27 10:08:39 -0600

    gitignore

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,7 +1,10 @@
-// generate a random function between 1 and 123
+import { useRef } from "react";
 
+// generate a random function between 1 and 123
 type Props = { image: string };
 
 export const RandomFox = ({ image }: Props): JSX.Element => {
-  return <img className="rounded-lg" width={320} height="auto" src={image} />;
+  const node = useRef<HTMLImageElement>(null);
+
+  return <img ref={node} className="rounded-lg" width={320} height="auto" src={image} />;
 };
